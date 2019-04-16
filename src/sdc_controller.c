b0e85199ce28a9f25326c05ab2f1526df6d3e2b1
root <arive284@fiu.edu>
2019-04-16 03:30:42 +0000

    adding code to C file

--- src/sdc_controller.c
+++ src/sdc_controller.c
@@ -4,6 +4,7 @@
 #include "extApiPlatform.h"
 #define MAX_EXT_API_CONNECTIONS=255
 #define NON_MATLAB_PARSING
+#define DO_NOT_USE_SHARED_MEMORY
 
 extern "C"{
 #include "extApi.h"
@@ -11,15 +12,22 @@ extern "C"{
 
 int main (int argc,char* argv[])
 {
+	int portNb=19997;
 
 	printf("Program started\n");
-	int clientID= simxStart((simxChar*)"127.0.0.1",19999,true,true,2000,5);
+	int clientID= simxStart((simxChar*)"131.94.186.11",portNb,true,true,2000,5);
 	if (clientID!=-1)
 	{
 		printf("connected to remote api server\n");
+		printf("\n Number of ID: %d",clientID);
+	 
 	}
-	simxFinish(clientID);
+	else{
+		printf("fail");
+	}
 	return 0;
+
+	
 }
 
 
