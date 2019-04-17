8de4002a212061d3078119631af06972e79f48d4
root <arive284@fiu.edu>
2019-04-17 20:27:18 +0000

    work pls

--- src/sdc_controller.c
+++ src/sdc_controller.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "extApi.c"
+#include "./connections/remoteApi/extApi.c"
 #include "extApi.h"
 #include "extApiPlatform.h"
 #include "extApiPlatform.c"
