2c7df5b13f125987585240e3e93d4225a412298a
gabadoo <gabsdrummond@gmail.com>
2019-04-17 03:49:26 +0000

    Cmake files in each build

---
+++ src/build/CMakeFiles/deepC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deepC.dir/sdc_controller.c.o"
+  "deepC.pdb"
+  "deepC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/deepC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
