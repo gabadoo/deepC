a768f77053326cad048ff63892d39877b50b1d1f
gabadoo <gabsdrummond@gmail.com>
2019-04-18 20:22:31 +0000

    Updated project directory

---
+++ lib/build/CMakeFiles/Remote.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/user/code/deepC/lib/remoteApi/extApi.c" "/home/user/code/deepC/lib/build/CMakeFiles/Remote.dir/remoteApi/extApi.c.o"
+  "/home/user/code/deepC/lib/remoteApi/extApiPlatform.c" "/home/user/code/deepC/lib/build/CMakeFiles/Remote.dir/remoteApi/extApiPlatform.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/build/CMakeFiles/Remote.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Remote.dir/remoteApi/extApi.c.o"
+  "CMakeFiles/Remote.dir/remoteApi/extApiPlatform.c.o"
+  "libRemote.pdb"
+  "libRemote.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Remote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
